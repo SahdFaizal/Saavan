c115f7b26a86d73b3e4f31bcb954a982ab3fb690
SahdFaizal <113359217+SahdFaizal@users.noreply.github.com>
2023-09-24 20:32:30 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "style.css";
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic){
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+
+function closeFullImg(pic){
+    fullImgBox.style.display = "none";
+}
